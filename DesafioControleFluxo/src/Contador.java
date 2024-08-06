b21c9d80e69a45f354fdd940e2a67bf614702a2b
rbsriobr <rbs.rio.br@gmail.com>
2024-08-05 22:50:45 -0300

    Commit do desafio controle de fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Contador {
+    public static void main(String[] args) //throws Exception
+    {
+        int parametro1;
+        int parametro2;
+        
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US) ;
+
+        System.out.println("Digite o primeiro parâmetro");
+        parametro1 = scan.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        parametro2 = scan.nextInt();
+
+        scan.close();
+        
+        try {
+            contar (parametro1, parametro2);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro de exceção: O parametro1 é maior que o parametro");
+            //e.printStackTrace();
+        }
+    }
+
+    static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException{
+        if(parametro1 > parametro2)
+            throw new ParametrosInvalidosException();
+        System.out.println("Impressão sequência do primeiro parâmetro");
+        for(int i=1; i <= parametro1; i++)
+        {
+            System.out.println("Imprimindo o número " + i);
+        }
+        System.out.println("Impressão sequência do segundo parâmetro");
+        for(int i=1; i <= parametro2; i++)
+        {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
